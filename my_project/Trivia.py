bf9c5aaba28f9bbedc2b491a4cb628e9fbe254c4
Jamal Chapman <jchapman0819@gmail.com>
2017-08-04 14:49:28 -0400

    completed with comments

--- my_project/Trivia.py
+++ my_project/Trivia.py
@@ -1,7 +1,11 @@
 from GetQuestions import GetQuestions
 from DisplayQuestions import DisplayQuestions
 
-
-x = DisplayQuestions(GetQuestions().getitem())
-x.answerEachQuestion()
+#here we ceate a new instance of the GetQuestions clsss
+triviaQuestions = GetQuestions().getitem()
+#there we crete a new instance of the Display Questions and use the
+#instance of the GetQuestions class to get the game started
+startGame = DisplayQuestions(triviaQuestions)
+#here we call the method answerEachQuestion which begins the game
+startGame.answerEachQuestion()
 
