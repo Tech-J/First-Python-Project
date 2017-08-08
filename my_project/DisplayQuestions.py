421056f25c2be3dff85651ad2849d0898a8c79a2
Jamal Chapman <jchapman0819@gmail.com>
2017-08-07 22:04:48 -0400

    Fix library path

--- my_project/DisplayQuestions.py
+++ my_project/DisplayQuestions.py
@@ -1,3 +1,5 @@
+import sys
+sys.path.insert(0, './lib/python3.6/site-packages')
 import GetQuestions
 import string
 import random
--- my_project/GetQuestions.py
+++ my_project/GetQuestions.py
@@ -1,3 +1,5 @@
+import sys
+sys.path.insert(0, './lib/python3.6/site-packages')
 import requests
 import json
 class GetQuestions(object):
